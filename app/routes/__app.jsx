29c2074f5d5ce6b59643e0f7ab26b5c2fb70938b
Marek Zelinka <mzelinka17@gmail.com>
2022-12-05 15:13:40 +0100

    update logo

--- app/routes/__app.jsx
+++ app/routes/__app.jsx
@@ -5,6 +5,21 @@ import {
 } from "@heroicons/react/20/solid";
 import { Form, Outlet, useSearchParams, useTransition } from "@remix-run/react";
 
+function Logo(props) {
+  const { className } = props;
+
+  return (
+    <svg
+      viewBox="0 0 49 42"
+      fill="currentColor"
+      aria-hidden
+      className={className}
+    >
+      <path d="M28.537 1.002V1H1.592v39.908H28.537v-.002c11.01-.17 19.87-9.03 19.87-19.952 0-10.921-8.86-19.782-19.87-19.952Zm-7.156 22.85v3.875c0 3.453-2.822 6.242-6.316 6.242-3.494 0-6.283-2.789-6.283-6.242V14.181c0-3.453 2.789-6.242 6.283-6.242s6.316 2.789 6.316 6.242v9.67Z" />
+    </svg>
+  );
+}
+
 function SearchForm() {
   const [searchParams] = useSearchParams();
   const username = searchParams.get("q");
@@ -54,11 +69,7 @@ export default function AppRoute() {
         <div className="mx-auto max-w-3xl px-2 sm:px-4 lg:px-8">
           <div className="flex h-16 justify-between">
             <div className="flex shrink-0 items-center px-2 md:px-0">
-              <img
-                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                alt=""
-                className="h-8 w-auto"
-              />
+              <Logo className="h-8 w-auto text-indigo-600" />
             </div>
             <div className="flex flex-1 items-center justify-end px-2 md:ml-6">
               <SearchForm />
