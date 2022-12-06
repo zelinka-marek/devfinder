0d3b260ed63c34d90c836648fb176ca96d681979
Marek Zelinka <mzelinka17@gmail.com>
2022-12-06 09:49:28 +0100

    cleanup

--- app/root.jsx
+++ app/root.jsx
@@ -8,17 +8,17 @@ import {
 } from "@remix-run/react";
 import tailwindStylesheetUrl from "./styles/tailwind.css";
 
-export const meta = () => {
+export function meta() {
   return {
     charset: "utf-8",
     title: "DevFinder",
     viewport: "width=device-width,initial-scale=1",
   };
-};
+}
 
-export const links = () => {
+export function links() {
   return [{ rel: "stylesheet", href: tailwindStylesheetUrl }];
-};
+}
 
 export default function App() {
   return (
